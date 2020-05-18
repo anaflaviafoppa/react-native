9d6a8334b7ad2781b577eaa1b035490bf4136dc2
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-05-18 23:19:09 +0100

    Restaurant Item

---
+++ src/components/RestaurantItem.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+const RestaurantItem = ({ sourceImg, name, review, rating }) => {
+  return (
+    <View>
+      <Image style={styles.tinyImage} source={{
+          uri: sourceImg,
+        }} />
+      <Text style={styles.titleStyle}>{name}</Text>
+      <View style={styles.viewSubtitleStyle}>
+        <Text style={styles.subtitleStyle}>{rating} Stars </Text>
+        <Text style={styles.subtitleStyle}>
+          {review > 1 ? `${review} Reviews` : `${review} Review`}{' '}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  titleStyle: {
+    fontSize: 30,
+  },
+  subtitleStyle: {
+    fontSize: 15,
+  },
+  viewSubtitleStyle: {
+    flexDirection: 'row',
+    color: '#D3D3D3',
+  },
+  tinyImage: {
+    width: 100,
+    height: 128,
+  },
+});
+
+export default RestaurantItem;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,47 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import RestaurantItem from '../components/RestaurantItem';
+import yelp from '../api/yeip';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const serchApi = async () => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 5,
+          term,
+          location: 'lisbon',
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (e) {
+      setErrorMessage('Something went wrong');
+    }
+  };
+
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermSubmit={() => console.log('term was submmit')}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-      />
+      <SearchBar term={term} onTermSubmit={serchApi} onTermChange={setTerm} />
       <Text>Search Screen</Text>
+      <Text>We have fount {results.length} results</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <FlatList
+        data={results}
+        renderItem={({ item }) => (
+          <RestaurantItem
+            sourceImg={item.image_url}
+            review={item.review_count}
+            name={item.name}
+            rating={item.rating}
+          />
+        )}
+        //keyExtractor={item => item.id}
+      />
     </View>
   );
 };
