4a584e4cd519a86e99872b40976d7c30266d24c4
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-05-18 20:15:04 +0100

    set App with react navigation v 05

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+// import * as React from 'react';
+// // import { NavigationContainer } from '@react-navigation/native';
+// // import { createStackNavigator } from '@react-navigation/stack';
+// import { createStackNavigator, createAppContainer } from 'react-navigation';
+// import SearchScreen from './src/screens/SearchScreen';
 
+//  const navigator = createStackNavigator(
+//    {
+//      Search: SearchScreen,
+//    },
+//    {
+//      initialRouteName: 'Search',
+//      defaultNavigationOptions: {
+//        title: 'BusinessSearch',
+//      },
+//    }
+//  );
+
+//  export default createAppContainer(navigator);
+
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import SearchScreen from './src/screens/SearchScreen';
 
-const navigator = createStackNavigator(
-  {
-    Search: SearchScreen,
-  },
-  {
-    initialRouteName: 'Search',
-    defaultNavigationOptions: {
-      title: 'BusinessSearch',
-    },
-  }
-);
 
-export default createAppContainer(navigator);
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Business Search" component={SearchScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
